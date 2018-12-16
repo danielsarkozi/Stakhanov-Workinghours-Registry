b09f94c6223d8979016434ee33e9a04ad4d963ea
Sarkoziajoarc <lokosguba@gmail.com>
2018-12-16 19:12:17 +0100

    @crossplatform

--- Stakhanov/src/main/java/hu/elte/Stakhanov/controller/RegistryController.java
+++ Stakhanov/src/main/java/hu/elte/Stakhanov/controller/RegistryController.java
@@ -17,6 +17,7 @@ import java.util.Objects;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author lokos
  */
+@CrossOrigin
 @RestController
 @RequestMapping("registries")
 public class RegistryController {
@@ -73,7 +75,6 @@ public class RegistryController {
         }
     }
     
-    //nem szükséges 
     @PostMapping("")
     public ResponseEntity<Registry> post(@RequestBody Registry registry) {
         Person person = authenticatedUser.getPerson();
--- Stakhanov/src/main/java/hu/elte/Stakhanov/controller/TeamController.java
+++ Stakhanov/src/main/java/hu/elte/Stakhanov/controller/TeamController.java
@@ -28,6 +28,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,6 +42,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author lokos
  */
+@CrossOrigin
 @RestController
 @RequestMapping("teams")
 public class TeamController {
